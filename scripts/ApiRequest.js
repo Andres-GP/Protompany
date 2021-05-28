87f7bf5c3ccbabbcaaa31d7291ee20224a5cf911
Andres Garcia <andresgarciapedreros@gmail.com>
2021-05-27 20:28:39 -0500

    Protampany allproyect was added

---
+++ scripts/ApiRequest.js
@@ -0,0 +1,83 @@
+
+
+
+/*1st cat*/
+fetch("https://aws.random.cat/meow")
+.then(res => res.json())
+.then(data => getRandomCats1(data))
+
+const cat_result1 = document.getElementById('cat_result')
+
+
+
+function getRandomCats1(data){
+	console.log(data)
+  const cat_result1 = document.querySelector('#fourthPage__apiRequestContainer__testimonial__catContainer1')
+
+  cat_result1.innerHTML = 
+  `<img 
+  style="
+  border-radius: 100px;
+  width: 80px;
+  height: 80px;" 
+  src="${data.file}"/> `
+}
+
+
+
+
+
+
+/*2nd cat*/
+
+fetch("https://aws.random.cat/meow")
+.then(res => res.json())
+.then(data => getRandomCats2(data))
+
+
+const cat_result2 = document.getElementById('cat_result')
+
+
+
+function getRandomCats2(data){
+	console.log(data)
+  const cat_result2 = document.querySelector('#fourthPage__apiRequestContainer__testimonial__catContainer2')
+
+  cat_result2.innerHTML = 
+  `<img 
+  style="
+  border-radius: 100px;
+  width: 80px;
+  height: 80px;" 
+  src="${data.file}"/> `
+}
+
+
+
+
+/*3rd cat*/
+
+fetch("https://aws.random.cat/meow")
+.then(res => res.json())
+.then(data => getRandomCats3(data))
+
+const cat_result3 = document.getElementById('cat_result')
+
+
+
+function getRandomCats3(data){
+	console.log(data)
+
+  const cat_result3 = document.querySelector('#fourthPage__apiRequestContainer__testimonial__catContainer3')
+
+  cat_result3.innerHTML = 
+  `<img 
+  style="
+  border-radius: 100px;
+  width: 80px;
+  height: 80px;" 
+  src="${data.file}"/> `
+}
+
+
+
---
+++ scripts/simonSays.js
@@ -0,0 +1,113 @@
+
+  const blue = document.getElementById('blue')
+      const violet = document.getElementById('violet')
+      const orange = document.getElementById('orange')
+      const green = document.getElementById('green')
+      const btnEmpezar = document.getElementById('btnEmpezar')
+      const ULTIMO_NIVEL = 10
+
+      class Juego {
+        constructor() {
+            this.inicializar()
+            this.generarSecuencia()
+            setTimeout(this.sigienteNivel, 800)
+        }
+
+        inicializar() {
+          this.sigienteNivel = this.sigienteNivel.bind(this)
+          this.elegirColor = this.elegirColor.bind(this)
+          btnEmpezar.classList.add('hide')
+          this.nivel = 1
+          this.colores = {
+            blue,
+            violet,
+            orange,
+            green
+        }
+        }
+        generarSecuencia(){
+          this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() * 4))
+          console.log(this.secuencia)
+        }
+        sigienteNivel(){
+          this.subnivel = 0
+          this.iluminarSecuencia()
+          this.agregarEventosClick()
+        }
+        iluminarSecuencia(){
+          for (let i = 0; i < this.nivel; i++) {
+            const color = this.transformarNumeroAColor(this.secuencia[i])
+            setTimeout(() => {this.iluminarColor(color)}, 1000 * i)
+
+          }
+        }
+        transformarNumeroAColor(num){
+          switch(num){
+            case 0: 
+             return 'blue'
+            case 1:
+             return 'violet'
+            case 2:
+              return 'orange'
+            case 3:
+              return 'green'
+          }
+        }
+        transformarColorANumero(color){
+          switch(color){
+            case 'blue': 
+             return 0
+            case 'violet':
+             return 1
+            case 'orange':
+              return 2
+            case 'green':
+              return 3
+          }
+        }
+        iluminarColor(color){
+          this.colores[color].classList.add('light')
+          setTimeout(() => this.apagarColor(color), 2000)
+        }
+        apagarColor(color){
+          this.colores[color].classList.remove('light')
+        }
+        agregarEventosClick(){
+          this.colores.blue.addEventListener('click', this.elegirColor)
+          this.colores.green.addEventListener('click', this.elegirColor)
+          this.colores.violet.addEventListener('click', this.elegirColor)
+          this.colores.orange.addEventListener('click', this.elegirColor)
+        }
+        eliminarEventosClick(){
+          this.colores.blue.removeEventListener('click', this.elegirColor)
+          this.colores.green.removeEventListener('click', this.elegirColor)
+          this.colores.violet.removeEventListener('click', this.elegirColor)
+          this.colores.orange.removeEventListener('click', this.elegirColor)
+        }
+        elegirColor(ev){
+          const nombreColor = ev.target.dataset.color
+          const numeroColor = this.transformarColorANumero(nombreColor)
+          this.iluminarColor(nombreColor)
+          if (numeroColor === this.secuencia[this.subnivel]){
+            this.subnivel++
+            if (this.subnivel === this.nivel) {
+              this.nivel++
+              this.eliminarEventosClick()
+              if (this.nivel = ULTIMO_NIVEL + 1) {
+                //Ganó!
+              } else {
+                setTimeout(this.sigienteNivel, 1000)
+              }
+            }
+          }else{
+            //Perdió
+          }
+        }
+      }
+
+      function empezarJuego() {
+        window.juego = new Juego()
+      }
+
+
+
